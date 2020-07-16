cd260695405fd9b3f11e006c1b8f395c5665843f
Travis Hilterbrand <Travis.Hilterbrand@five9.com>
2020-07-16 11:30:54 -0700

    Add tippy examples

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'
+
 import './App.css';
+
+const ShowDelay = 1000;
+
+const StringContent = () => (
+  <Tippy content="Hello" delay={ShowDelay}>
+    <button>My button</button>
+  </Tippy>
+);
+const JSXContent = () => (
+  <Tippy content={<span>Tooltip</span>} placement={'top'} delay={ShowDelay}>
+    <button>My button</button>
+  </Tippy>
+);
+class DynamicContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.onInterval = this.onInterval.bind(this);
+  }
+  onInterval() {
+    this.setState({ count: this.state.count + 1 });
+  }
+  componentDidMount() {
+    this.timerId = setInterval(this.onInterval, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+  render() {
+    return (
+      <Tippy content={'Dynamic ' + this.state.count} placement={'top'} delay={ShowDelay}>
+        <button>Dynamic</button>
+      </Tippy>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StringContent />
+      <JSXContent />
+      <div style={{ height: 200 }} />
+      <StringContent />
+      <JSXContent />
+      <DynamicContent />
     </div>
   );
 }
